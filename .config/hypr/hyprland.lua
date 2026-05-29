1e4c76f216b4901b5487d5ae1084c39f6969af90
Spooky <spooky@mail.com>
2026-05-29 21:02:47 +0530

    nothing

--- .config/hypr/hyprland.lua
+++ .config/hypr/hyprland.lua
@@ -257,12 +257,15 @@ hl.window_rule({
 })
 
 --- fix some dragging issues with XWayland ---
-hl.window_rule({ name  = "fix-xwayland-drags", match = { class      = "^$",
-title      = "^$",
-xwayland   = true,
-float      = true,
-fullscreen = false,
-pin        = false,
+hl.window_rule({ 
+    name  = "fix-xwayland-drags", 
+    match = { 
+        class      = "^$",
+        title      = "^$",
+        xwayland   = true,
+        float      = true,
+        fullscreen = false,
+        pin        = false,
     },
 
     no_focus = true,
