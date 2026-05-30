1e417546c1b1cf748de3aa8363c894a281e2b3f5
Spooky <spooky@mail.com>
2026-05-30 17:25:21 +0530

    removed nvim configs since there is a seperate repo for it

--- .config/nvim/lua/config/colors.lua
+++
@@ -1,6 +0,0 @@
-vim.cmd("colorscheme matugen")
-vim.api.nvim_create_autocmd("Signal", {
-    pattern = "SIGUSR1",
-    command = "colorscheme matugen",
-})
-vim.api.nvim_set_hl( 0, "Normal", { bg = "none" })
